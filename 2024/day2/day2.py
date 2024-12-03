3435b42f0b3e1f013929dd50b00a9c69489b1adf
MaxDistructo <MaxDistructo@users.noreply.github.com>
2024-12-02 18:30:25 -0600

    Add Day 2 part 2

--- 2024/day2/day2.py
+++ 2024/day2/day2.py
@@ -3,10 +3,9 @@ with open("input.txt", "r") as f:
 
 safe_reports = 0
 
-for line in lines:
+def check_line(line: str) -> bool:
     prev = -1
     direction = ""
-    was_safe = True
     for val in line.split(" "):
         num = int(val)
         if(prev == -1):
@@ -17,20 +16,34 @@ for line in lines:
             if(direction == ""):
                 direction = "inc" 
             if(direction != "inc"):
-                was_safe = False
-                break
+                return False
         elif num - prev < 0 and abs(num - prev) <= 3: 
             if(direction == ""):
                 direction = "dec" 
             if(direction != "dec"):
-                was_safe = False
-                break
+                return False
         else:
             # Ignore this line as it cannot meet the criteria
-            was_safe = False
-            break
+            return False
         prev = num
-    if was_safe:
+    return True
+
+fail_lines = []
+
+for line in lines:
+    # Process directly
+    if check_line(line):
         safe_reports += 1
+    else:
+        fail_lines.append(line)
+
+# Brute force check if any 1 number can be removed and pass the test
+for line in fail_lines:
+    split_line = str(line).split(" ")
+    for i in range(len(split_line)):
+        new_line = split_line[:i] + split_line[i+1:]
+        if check_line(" ".join(new_line)):
+            safe_reports += 1
+            break
 
 print(safe_reports)
